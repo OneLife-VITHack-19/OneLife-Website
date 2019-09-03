b93416220a64e13c6f461fb7666f20c14b8e57b0
Prakhar Soni <prakhar0912@gmail.com>
2019-09-03 23:06:44 +0530

    final(without all images)

--- main.js
+++ main.js
@@ -1,9 +1,54 @@
 var controller = new ScrollMagic.Controller();
 new ScrollMagic.Scene({
     triggerElement: ".nav-logo2",
-    triggerHook: 0.9, // show, when scrolled 10% into view
-    duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-    offset: 50 // move trigger to center of element
+    triggerHook: 0.8, 
+    duration: 800, 
+    offset: 100 
 })
 .setClassToggle(".nav-logo2", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: ".exp",
+    triggerHook: 0.8, 
+    duration: 800, 
+    offset: 50 
+})
+.setClassToggle(".exp", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: ".text2",
+    triggerHook: 0.8, 
+    duration: 800,
+    offset: 90
+})
+.setClassToggle(".text2", "show-test2") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: ".nav-logo3",
+    triggerHook: 0.8, 
+    duration: 800, 
+    offset: 100
+})
+.setClassToggle(".nav-logo3", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: ".gloves",
+    triggerHook: 0.8,
+    duration: 800,
+    offset: 50 
+})
+.setClassToggle(".gloves", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: ".text3",
+    triggerHook: 0.8,
+    duration: 800, 
+    offset: 90
+})
+.setClassToggle(".text3", "show-test3") // add class to reveal
 .addTo(controller);
